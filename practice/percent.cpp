ff16499a73030a63b0f6077f2b808221b8e7aa8d
Ayush Rusiya <ayushrusiya9@gmail.com>
2025-03-07 10:19:16 +0530

    new file

---
+++ practice/percent.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    int ph,m,c;
+    cout<<"Enter physics marks ";
+    cin>>ph;
+    cout<<"Enter maths marks ";
+    cin>>m;
+    cout<<"Enter chemistry marks ";
+    cin>>c;
+
+     int total = ph + m + c ;
+     int result = total / 3;
+
+     if(result >= 60 && result <= 100){
+        cout<<"First Division ";
+     }
+     else if(result >= 50 && result <= 59){
+        cout<<"Second Division ";
+     }
+     else if(result >= 35 && result <= 50)
+     {
+         cout<<"Thired Division ";
+     }
+     else{
+        fail
+     }
+    // cout<<result;
+
+
+
+}
